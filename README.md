be08c48c5017ed17fbfde1cebda2b4ce4d3074de
Tom Sarver <tom@tomsarver.dev>
2021-11-21 14:37:25 -0500

    updating N design

--- README.md
+++
@@ -1,3 +0,0 @@
-This project is all about the letter N!
-
-To view the letter, download the project and view the index.php page from a web browser.
--- js/backgroundResize.js
+++
@@ -1,8 +0,0 @@
-document.getElementsByTagName("BODY").onresize = function() {resizeFunction()};
-
-
-function resizeFunction() {
-	var text = "TESTING";
-
-	document.getElementById("mainTitle").innerHTML = txt;
-}
